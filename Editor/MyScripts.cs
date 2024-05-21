b04a932884cf3c0514732cc9d56a55de521d431f
Chaipat J. <work.chaipat@gmail.com>
2024-05-21 09:46:05 +0700

    chore ~

--- Editor/MyScripts.cs
+++ Editor/MyScripts.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
 
+namespace AppleBoiy.HelloUnity
+{
 public class MyScripts : MonoBehaviour
 {
     public void Greet()
     {
         Debug.Log("Hello, World!");
     }
+    
+    public static void GreetStatic()
+    {
+        Debug.Log("Hello, World! (Static)");
+    }
+}
+
 }
